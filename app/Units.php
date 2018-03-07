3cf02e5d986ae599ac10f41373b50fbad62887f7
Rafael <rvila@revolutionvisualarts.com>
2018-03-06 18:07:37 -0600

    Adding license, links and author information

--- app/Units.php
+++ app/Units.php
@@ -12,7 +12,26 @@ namespace raphievila\Tools;
 /**
  * Description of Units
  *
- * @author Rafael Vila <rafael@stealthproducts.com>
+ * @author Rafael Vila <rvila@revolutionvisualarts.com>
+ * @license: GNU GENERAL PUBLIC LICENSE
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * for more information go to:
+ * https://github.com/raphievila/units/blob/master/LICENSE
+ * 
+ * @link: github:   https://github.com/raphievila/units/
+ *        website:  https://revolutionvisualarts.com
  */
 
 use Utils\Utils;
